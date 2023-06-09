b5a333ebc9d417da789e76e8ceefda1e79b7eb14
Ayaan <53174223+astro648@users.noreply.github.com>
2023-06-09 16:38:47 -0700

    can modify helloworld from inside editor

--- BallTrack/Assets/HelloWorld.cs
+++ BallTrack/Assets/HelloWorld.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class HelloWorld : MonoBehaviour
 {
+    public string myMessage;
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hello World!");
+        Debug.Log(myMessage);
     }
 
     // Update is called once per frame
